3c864e92ef4572dd30d72f335264d8f7e2051971
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2024-11-30 06:06:02 +0100

    Fix issues with vitest

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,12 @@ import { configDefaults } from 'vitest/config';
 export default defineConfig({
   plugins: [eslintPlugin()],
   test: {
-    globals: true, // Wenn du globale Funktionen wie "describe" und "it" verwenden möchtest
-    environment: 'jsdom', // Standardumgebung für Tests
-    exclude: [...configDefaults.exclude], // Tests ausschließen (optional)
+    globals: false, // If you want to use global functions such as "describe" and "it"
+    environment: 'jsdom', // Standard environment for tests
+    exclude: [...configDefaults.exclude], // Exclude tests (optional)
     coverage: {
       enabled: true,
-      provider: 'v8', // v8, c8 Oder 'istanbul', wenn du Coverage möchtest
+      provider: 'v8', // v8, c8 Or 'istanbul' if you want coverage
       exclude: [...configDefaults.coverage.exclude, 'examples/**'], // Exclude the "examples" folder from coverage
       thresholds: {
         statements: 100,
